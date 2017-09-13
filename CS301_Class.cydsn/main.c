daca64a873387f102b44cc57ced90c7e4c5047f4
Mark Yep <myep112@UoA.auckland.ac.nz>
2017-09-13 16:58:33 +1200

    fixing the turn algortihm

--- CS301_Class.cydsn/main.c
+++ CS301_Class.cydsn/main.c
@@ -1,10 +1,7 @@
 /* ========================================
- * Fully working code: 
- * PWM      : 
- * Encoder  : 
- * ADC      :
- * USB      : port displays speed and position.
- * CMD: "PW xx"
+ * Group 6 COMPSYS 301 Project
+ * Line Following Robot
+ *
  * Copyright Univ of Auckland, 2016
  * All Rights Reserved
  * UNPUBLISHED, LICENSED SOFTWARE.
@@ -20,40 +17,48 @@
 #include <project.h>
 #include <ADC_SAR.h>
 #include <CONTROL.h>
-
-#define RXSTRINGSIZE 255
-#define BUF_SIZE 64 // USBUART fixed buffer size
-
-#define SOP 0xaa
+#include "defines.h"
+#include "vars.h"
 //* ========================================
 char rf_string[RXSTRINGSIZE];
-char line[BUF_SIZE], battery_voltage_string[BUF_SIZE], test[BUF_SIZE], entry[BUF_SIZE], motor_line_1[BUF_SIZE], motor_line_2[BUF_SIZE];
+char line[BUF_SIZE], test[BUF_SIZE], entry[BUF_SIZE], motor_line_1[BUF_SIZE], motor_line_2[BUF_SIZE];
 uint8 usb_buffer[BUF_SIZE];
 //* ========================================
 int mode = 0;
 int count = 0;
 int start_count = 0;
-int usb_output = 1;
+int usb_output = 0;
 //* ========================================
-int motor_1_distance = 0; //global variables that keep track of distance travelled by robot's two wheels
-int motor_2_distance = 0; //distances are in millimetres
+int motor_1_distance = 0;                                            // global variables that keep track of distance travelled by robot'swheels
+int motor_2_distance = 0;                                               // distances are in millimetres
 //* ========================================
-//Arrays for light sensors, index value 0 is not used (makes it easier to link index number to sensor number)
-int max_value[] = {0,0,0,0,0,0,0};             //max value sensed by each light sensor (ignore index value 0)
-int min_value[] = {0,0,0,0,0,0,0};             //min value sensed by each light sensor (ignore index value 0)
-int ADC_value[] = {0,0,0,0,0,0,0};             //current ADC value from each light sensor (ignore index value 0)
-int is_under_line[] = {0,0,0,0,0,0,0};       //boolean value for determining whether or not a light sensor is under the line or not (ignore index value 0)
+// arrays for light sensors
+int max_value[] = {0,0,0,0,0,0,0};                          // max value sensed by each light sensor 
+int min_value[] = {0,0,0,0,0,0,0};             // min value sensed by each light sensor 
+int ADC_value[] = {0,0,0,0,0,0,0};             // current ADC value from each light sensor 
+int is_under_line[] = {0,0,0,0,0,0,0};         // boolean value for holding if a light sensor is under the line
 //* ========================================
-//Initialising motor speed values
-int motor_1_default_speed = 80;
-int motor_2_default_speed = 80;
-int motor_1_speed = 80;
-int motor_2_speed = 80;
+// initialising motor speed values
+int voltage = 0;
+float initial_speed = 127;
+int motor_1_default_speed = 127;
+int motor_2_default_speed = 127;
+int motor_1_speed = 127;
+int motor_2_speed = 127;
+
+int back_turn_speed = 52; // for backwards turning wheel
+int forward_turn_speed = 37; // for forwards turning wheel
+int motor_correction_speed = 25; // for correction
+int motor_backwards_speed = 170; // for reversing
 //* ========================================
-//Flags for turns
+// flags for turns
 int turning_left = 0;
 int turning_right = 0;
+int is_reverse = 0;
+int is_turning_left = 0;
+int is_turning_right = 0;
 //* ========================================
+// function definitions
 void print_light_sensor_values();
 void sensor_is_under_line(int sensorNum);
 int get_battery_voltage();
@@ -68,13 +73,10 @@ void handle_turns();
 void curves_mode();
 void turns_mode();
 void calculate_distance_travelled();
+void set_speeds();
 void check_mode();
 //* ========================================
-#include "defines.h"
-#include "vars.h"
-//* ========================================
 int main() {
-// --------------------------------    
 // ----- INITIALIZATIONS ----------
     CYGlobalIntEnable;
 
@@ -90,8 +92,8 @@ int main() {
     Clock_PWM_Start(); // Start clock for PWM
     PWM_1_Start();
     PWM_2_Start();
-    PWM_1_WriteCompare(motor_1_speed);
-    PWM_2_WriteCompare(motor_2_speed);
+    PWM_1_WriteCompare(127); // move motor1 forward
+    PWM_2_WriteCompare(127);
 
 // ------ADC SETUP ----------------      
     ADC_Start();
@@ -102,60 +104,66 @@ int main() {
     USBUART_Start(0,USBUART_5V_OPERATION);
     UART_Start();
     isrRF_RX_Start();
-    
-    while(1) {
-        //Update each sensor values of max_value, ADC_value and is_under_line
+  
+    while(1) { 
+        set_speeds();
+        
+        // update each sensor values of max_value, ADC_value and is_under_line
         int m;
         for (m = 1; m < 7; m++){
             sensor_is_under_line(m);
         }
         
         check_mode();
-        
+        turns_mode();
+        /*
+        if(flag_rx == 1) {
+            usb_put_char(UART_GetChar());
+        }
+        */
+        /*
         if (mode == 0) {
-            curves_mode();
+            curves_mode(); 
         } else if (mode == 1) {
             turns_mode();   
-        }   
+        } else if (mode == 2) {
+            continue;
+        } else if (mode == 3) {
+            continue;
+        }
+        */
         /*
-        int voltage = get_battery_voltage();
-        itoa(voltage, battery_voltage_string, 10);
-        usb_put_string("Battery Voltage: ");
-        usb_put_string(battery_voltage_string);
-        usb_put_string("\n\r");
-    
-        int complete_structure = handle_radio_data();
+        int complete_structure = handle_radio_data();     
         if (complete_structure == 1) {
             int8 strength = system_state.rssi;
             int16 xpos = system_state.robot_xpos;
             int16 ypos = system_state.robot_ypos;
             int16 orient = system_state.robot_orientation;
-                
-            if (orient > 0) {                   
-                itoa(strength, line, 10);
-                usb_put_string("RSSI: ");
-                usb_put_string(line);
-                usb_put_string("\n\r");
-                
-                itoa(xpos, line, 10);
-                usb_put_string("XPOS: ");
-                usb_put_string(line);
-                usb_put_string("\n\r");
-                
-                itoa(ypos, line, 10);
-                usb_put_string("YPOS: ");
-                usb_put_string(line);
-                usb_put_string("\n\r");
-                
-                itoa(orient, test, 10);
-                usb_put_string("Orientation: ");
-                usb_put_string(test);
-                usb_put_string("\n\r");
-            }
+            
+                   
+            itoa(strength, line, 10);
+            usb_put_string("RSSI: ");
+            usb_put_string(line);
+            usb_put_string("\n\r");
+            
+            itoa(xpos, line, 10);
+            usb_put_string("XPOS: ");
+            usb_put_string(line);
+            usb_put_string("\n\r");
+            
+            itoa(ypos, line, 10);
+            usb_put_string("YPOS: ");
+            usb_put_string(line);
+            usb_put_string("\n\r");
+            
+            itoa(orient, test, 10);
+            usb_put_string("Orientation: ");
+            usb_put_string(test);
+            usb_put_string("\n\r");
+            
         }
-    
-    */
-    calculate_distance_travelled();
+        */
+        calculate_distance_travelled();
     }
 }
 
@@ -288,6 +296,10 @@ int get_battery_voltage() {
             }
         } 
     int voltage = (ADC_value*8*1000)/4096;  
+    itoa(ADC_value, line, 10);
+    usb_put_string("voltage: ");
+    usb_put_string(line);
+    usb_put_string("\n\r"); 
     return voltage;
 }
 //* ========================================
@@ -301,7 +313,7 @@ int handle_radio_data() {
             memcpy(&system_state, rf_string, PACKETSIZE);
             count = 0;
                 
-        rxReceived = 1;    
+            rxReceived = 1;    
         }
         
         data = UART_GetChar();
@@ -316,8 +328,10 @@ int handle_radio_data() {
             rf_string[count] = data;
         }
         count++;
-        flag_rx = 0;
+        flag_rx = 0;        
     }
+    
+    
     return rxReceived;
 }
 //* ========================================
@@ -386,45 +400,48 @@ void handle_usb() {
 void turn_left(){
     // function to turn the robot to the left
     // change to reasonable turning speed for both motors
-    
-    PWM_1_WriteCompare(180); //move motor1 backward
-    PWM_2_WriteCompare(65); //move motor2 forward  
+    PWM_1_WriteCompare(motor_1_default_speed + back_turn_speed); // move motor1 backward
+    PWM_2_WriteCompare(motor_2_default_speed - forward_turn_speed); // move motor2 forward  
 }
 //* ========================================
 void turn_right() {
     // function to turn the robot to the right
     // change to reasonable turning speed for both motors
-    
-    PWM_1_WriteCompare(65); //move motor1 forward
-    PWM_2_WriteCompare(180); //move motor2 backward 
+
+    PWM_1_WriteCompare(motor_1_default_speed - forward_turn_speed); // move motor1 forward
+    PWM_2_WriteCompare(motor_2_default_speed + back_turn_speed); // move motor2 backward 
 }
 //* ========================================
 void go_straight() {
     // function to make the robot go straight
-    // change to default speed for both motors
-    
+    // change to default speed for both motors  
     motor_1_speed = motor_1_default_speed;
     motor_2_speed = motor_2_default_speed;
-    PWM_1_WriteCompare(motor_1_speed);
-    PWM_2_WriteCompare(motor_2_speed);
+    PWM_1_WriteCompare(motor_1_default_speed);
+    PWM_2_WriteCompare(motor_2_default_speed);
 }
 //* ========================================
 void turns_mode() { 
     // function to handle turns mode
     
     if (turning_left == 1) {
-        if (is_under_line[1] == 1 && is_under_line[2] == 1 && is_under_line[3] == 1 && is_under_line[4] == 1) {
+        if (is_under_line[1] == 1 || is_under_line[2] == 1) {
             turning_left = 0;
-            go_straight();
-        } else {
-            turn_left(); 
+            motor_1_speed = motor_1_default_speed;
+            motor_2_speed = motor_2_default_speed + motor_correction_speed;
+            PWM_1_WriteCompare(motor_1_speed);
+            PWM_2_WriteCompare(motor_2_speed);
+        } else {   
+            turn_left();
         }
     }
-    
-    if (turning_right == 1) {
-        if (is_under_line[1] == 1 && is_under_line[2] == 1 && is_under_line[3] == 1 && is_under_line[4] == 1) {
+    else if (turning_right == 1) {
+        if (is_under_line[1] == 1 || is_under_line[2] == 1) {
             turning_right = 0;
-            go_straight();
+            motor_1_speed = motor_1_default_speed + motor_correction_speed;
+            motor_2_speed = motor_2_default_speed;       
+            PWM_1_WriteCompare(motor_1_speed);
+            PWM_2_WriteCompare(motor_2_speed);
         } else {   
             turn_right();
         }
@@ -437,25 +454,25 @@ void turns_mode() {
         go_straight();
     }
     else if (is_under_line[1] == 1 && is_under_line[2] == 0) {
-        motor_1_speed = 105;
-        motor_2_speed = 75;       
+        motor_1_speed = motor_1_default_speed + motor_correction_speed;
+        motor_2_speed = motor_2_default_speed;       
         PWM_1_WriteCompare(motor_1_speed);
         PWM_2_WriteCompare(motor_2_speed);
     }
     else if (is_under_line[1] == 0 && is_under_line[2] == 1) {
-        motor_1_speed = 75;
-        motor_2_speed = 105;
+        motor_1_speed = motor_1_default_speed;
+        motor_2_speed = motor_2_default_speed + motor_correction_speed;
         PWM_1_WriteCompare(motor_1_speed);
         PWM_2_WriteCompare(motor_2_speed);
     }
     
     // handle 90 degree turns
     //CASE 3
-    else if (is_under_line[1] == 0 && is_under_line[2] == 0 && is_under_line[3] == 1 && is_under_line[4] == 1 && is_under_line[5] == 0 && is_under_line[6] == 1) {
+    if (is_under_line[1] == 0 && is_under_line[2] == 0 && is_under_line[3] == 1) {
         turning_left = 1;    
     } 
     //CASE 4
-    else if (is_under_line[1] == 0 && is_under_line[2] == 0 && is_under_line[3] == 0 && is_under_line[4] == 1 && is_under_line[5] == 1 && is_under_line[6] == 1) {
+    else if (is_under_line[1] == 0 && is_under_line[2] == 0 && is_under_line[5] == 1) {
         turning_right = 1;
     }
 }
@@ -471,22 +488,22 @@ void curves_mode() {
     }
     //CASE 10
     else if (is_under_line[1] == 1 && is_under_line[2] == 0) { // sensor 1 is on the line, sensor 2 is off the line    
-        motor_1_speed = 105; // decrease motor1 speed
-        motor_2_speed = 75; // increase motor2 speed        
+        motor_1_speed = motor_1_default_speed + motor_correction_speed; // decrease motor1 speed
+        motor_2_speed = motor_2_default_speed; // increase motor2 speed        
         PWM_1_WriteCompare(motor_1_speed);
         PWM_2_WriteCompare(motor_2_speed);
     }
     //CASE 9
     else if (is_under_line[1] == 0 && is_under_line[2] == 1) { // sensor 1 is off the line, sensor 2 is on the line
-        motor_1_speed = 75; // increase motor1 speed
-        motor_2_speed = 105; // decrease motor2 speed
+        motor_1_speed = motor_1_default_speed; // increase motor1 speed
+        motor_2_speed = motor_2_default_speed + motor_correction_speed; // decrease motor2 speed
         PWM_1_WriteCompare(motor_1_speed);
         PWM_2_WriteCompare(motor_2_speed);
     }
     //CASE 8
     else if (is_under_line[1] == 0 && is_under_line[2] == 0) { // both front sensors off the line
-        motor_1_speed = 170; // slow reverse
-        motor_2_speed = 170; // slow reverse
+        motor_1_speed = motor_backwards_speed; // slow reverse
+        motor_2_speed = motor_backwards_speed; // slow reverse
         PWM_1_WriteCompare(motor_1_speed);
         PWM_2_WriteCompare(motor_2_speed);
     }
@@ -516,18 +533,62 @@ void calculate_distance_travelled(){
         usb_put_string("\n\r"); 
     }
 }    
+void set_speeds() {
+    voltage = get_battery_voltage();
+
+    if (voltage < 7300) {
+        initial_speed = 127;
+        LED_Write(1);
+    } else {
+        initial_speed = (float)((float)((float)voltage / 160.0) + (float)(135.0 / 4.0));
+        if (voltage < 7700) {
+            motor_correction_speed = 26;
+        } else if (voltage < 8100) {
+            motor_correction_speed = 25;
+        } else {
+            motor_correction_speed = 24;
+        }
+        if (voltage < 7600) {
+            forward_turn_speed = 39;
+        } else if (voltage < 7900) {
+            forward_turn_speed = 38;
+        } else if (voltage < 8200) {
+            forward_turn_speed = 37;
+        } else {
+            forward_turn_speed = 36;
+        }
+        if (voltage < 7540) {
+            back_turn_speed = 55;
+        } else if (voltage < 7780) {
+            back_turn_speed = 54;
+        } else if (voltage < 8020) {
+            back_turn_speed = 53;
+        } else if (voltage < 8260) {
+            back_turn_speed = 52;
+        } else {
+            back_turn_speed = 51;
+        }
+    }
+    motor_1_default_speed = (int) initial_speed;
+    motor_2_default_speed = (int) initial_speed; 
+    motor_backwards_speed = (initial_speed * 2) + 10;
+}
+
 //* ========================================
 void check_mode() {
     // checks mode switches to determine mode
-    
-    if ((mode_switch0_Read() == 0) && (mode_switch1_Read() == 0)) {
-        mode = 0;
-    } else if ((mode_switch0_Read() == 0) && (mode_switch1_Read() == 1)) {
-        mode = 1;
-    } else if ((mode_switch0_Read() == 1) && (mode_switch1_Read() == 0)) {
-        mode = 2;
-    } else if ((mode_switch0_Read() == 1) && (mode_switch1_Read() == 1)) {
-        mode = 3;
+    if(mode_switch2_Read() == 0){
+        if(mode_switch3_Read() == 0){
+            mode = 0;
+        } else if(mode_switch3_Read() == 1) {
+            mode = 1;
+        }
+    } else if(mode_switch2_Read() == 1) {
+        if(mode_switch3_Read() == 0){
+            mode = 2;
+        } else if(mode_switch3_Read() == 1) {
+            mode = 3;
+        }
     }
 }
 //* ========================================
